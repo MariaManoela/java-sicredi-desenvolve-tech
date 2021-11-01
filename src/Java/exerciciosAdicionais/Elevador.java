1286bd464ef072ed155c37e43e3f3f373dbbdc1e
Maria Manoela Martins Vieira da Cunha <maria_manoela@sicredi.com.br>
2021-11-01 14:52:08 -0300

    Implement entra method

--- src/Java/exerciciosAdicionais/Elevador.java
+++ src/Java/exerciciosAdicionais/Elevador.java
@@ -9,10 +9,17 @@ public class Elevador {
         public void inicializa (int capacidade, int totalAndares){
             andarAtual = 0;
             pessoasPresentes = 0;
+            this.capacidade = capacidade;
+            this.totalAndares = totalAndares;
         }
 
         public void entra (){
-
+            if(pessoasPresentes < capacidade){
+                pessoasPresentes += 1;
+                System.out.println("Total de pessoas: " +pessoasPresentes);
+            }else{
+                System.out.println("Capacidade máxima atingida!");
+            }
         }
 
         public void sai (){
