369d8a9e2f61a2329074532f4d6427a1313ae1e0
Maria Manoela Martins Vieira da Cunha <maria_manoela@sicredi.com.br>
2021-11-01 14:17:48 -0300

    Implement inicializa method

---
+++ src/Java/exerciciosAdicionais/Elevador.java
@@ -0,0 +1,31 @@
+package Java.exerciciosAdicionais;
+
+public class Elevador {
+        private int andarAtual;
+        private int totalAndares;
+        private int capacidade;
+        private int pessoasPresentes;
+
+        public void inicializa (int capacidade, int totalAndares){
+            andarAtual = 0;
+            pessoasPresentes = 0;
+        }
+
+        public void entra (){
+
+        }
+
+        public void sai (){
+
+        }
+
+        public void sobe (){
+
+        }
+
+        public void desce (){
+
+        }
+
+
+}
