7884c128ef0411502c3df9b1ec7cdda732bd5572
Maria Manoela <manoelamvc@hotmail.com>
2021-11-03 11:44:45 -0300

    Adding method

--- src/Java/exerciciosAdicionais/exercicio02.java
+++ src/Java/exerciciosAdicionais/exercicio02.java
@@ -2,6 +2,11 @@ package Java.exerciciosAdicionais;
 
 public class exercicio02 {
     public static void main(String[] args){
+        char[] lista = new char[50];
+        int repeticoes;
+    }
+
+    public void concatenacao (){
 
     }
 }
