11abcb685c6d92b649d1846a5f5744fcf4a33153
Maria Manoela <manoelamvc@hotmail.com>
2021-11-03 10:52:08 -0300

    Adding set and get methods to main class

--- src/Java/exerciciosAdicionais/Main.java
+++ src/Java/exerciciosAdicionais/Main.java
@@ -1,5 +1,18 @@
 package Java.exerciciosAdicionais;
 
 public class Main {
+    public static void main(String[] args){
+        Elevador elevador = new Elevador();
+        elevador.setAndarAtual(0);
+        elevador.getAndarAtual();
 
+        elevador.setCapacidade(15);
+        elevador.getCapacidade();
+
+        elevador.setPessoasPresentes(0);
+        elevador.getPessoasPresentes();
+
+        elevador.setTotalAndares(20);
+        elevador.getTotalAndares();
+    }
 }
