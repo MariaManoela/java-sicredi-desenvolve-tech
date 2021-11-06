8c951808777630ccf5c0fd0583982c4c16f49b2e
Maria Manoela <manoelamvc@hotmail.com>
2021-11-06 10:57:21 -0300

    Correcting main class

--- src/Java/aula04/exercicio03/Main.java
+++ src/Java/aula04/exercicio03/Main.java
@@ -3,15 +3,15 @@ package Java.aula04.exercicio03;
 public class Main {
     public static void main(String[] args) {
         Ingresso ingresso1 = new Ingresso(20);
-        Normal norrmal1 = new Normal(20);
+        Normal normal1 = new Normal(20);
         Vip vip1 = new Vip(20, 10);
         CamaroteInferior camaroteinf1 = new CamaroteInferior(20, 10, "H2");
-        CamaroteSuperior camarotesuo1 = new CamaroteSuperior(20, 10, 15);
+        CamaroteSuperior camarotesup1 = new CamaroteSuperior(20, 10, 15);
 
         ingresso1.imprimeValor();
-        norrmal1.imprimeValor();
+        normal1.imprimeValor();
         vip1.imprimeValor();
         camaroteinf1.imprimeValor();
-        camarotesuo1.imprimeValor();
+        camarotesup1.imprimeValor();
     }
 }
