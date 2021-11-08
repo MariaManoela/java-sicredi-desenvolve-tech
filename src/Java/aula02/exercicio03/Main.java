8d60a0c0b679369a0548fa892632e25caa91c1ce
Maria Manoela <manoelamvc@hotmail.com>
2021-11-08 09:40:25 -0300

    Adding new client

--- src/Java/aula02/exercicio03/Main.java
+++ src/Java/aula02/exercicio03/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         Cliente cliente1 = new Cliente();
         Cliente cliente2 = new Cliente();
+        Cliente cliente3 = new Cliente();
 
         cadastro.cadastraNome(cliente1);
         cadastro.cadastraCpf(cliente1);
@@ -18,6 +19,13 @@ public class Main {
         cadastro.cadastraIdade(cliente2);
         cadastro.cadastraGenero(cliente2);
         cadastro.cadastraCliente(cliente2);
+
+        cadastro.cadastraNome(cliente3);
+        cadastro.cadastraCpf(cliente3);
+        cadastro.cadastraIdade(cliente3);
+        cadastro.cadastraGenero(cliente3);
+        cadastro.cadastraCliente(cliente3);
+
         cadastro.imprimeClientes();
     }
 }
