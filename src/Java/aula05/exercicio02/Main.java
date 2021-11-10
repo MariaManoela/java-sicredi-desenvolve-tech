e6b1fb996966531afa65b71ee28f5b1f9135264d
Maria Manoela <manoelamvc@hotmail.com>
2021-11-10 10:48:34 -0300

    Finishing exercicio02

--- src/Java/aula05/exercicio02/Main.java
+++ src/Java/aula05/exercicio02/Main.java
@@ -11,13 +11,20 @@ public class Main {
         lista.add(30);
         lista.add(40);
 
-//        novaLista(lista, (inteiro1, inteiro2) -> inteiro1 + inteiro2);
-        System.out.println(novaLista(lista, (inteiro1, inteiro2) -> inteiro1 + inteiro2));
+        System.out.println(novaLista(lista, (int n, int n2) -> n + n2));
+        System.out.println(novaLista(lista, (int n, int n2) -> (n + n2) / 2));
+        System.out.println(novaLista(lista, (int n, int n2) -> {
+            if (n > n2){
+                return n;
+            }else{
+                return n2;
+            }
+        }));
     }
 
     public static List novaLista(ArrayList<Integer> l, Inteiros inteiros){
         List novaLista = new ArrayList();
-        for (int i = 0; i < novaLista.size()-1; i++){
+        for (int i = 0; i < l.size()-1; i++){
             novaLista.add(inteiros.resultante(l.get(i), l.get(i+1)));
         }
         return novaLista;
